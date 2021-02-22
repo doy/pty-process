edfffc8c490b9d179a62901dfb874a85ab0984e3
Jesse Luehrs <doy@tozt.net>
2021-02-22 03:43:49 -0500

    refactor

--- src/command.rs
+++ src/command.rs
@@ -1,87 +1,23 @@
 use crate::error::*;
 
-use std::os::unix::io::{AsRawFd as _, FromRawFd as _};
-use std::os::unix::process::CommandExt as _;
+use ::std::os::unix::io::AsRawFd as _;
 
-pub trait Command {
-    fn spawn_pty(&mut self, size: Option<&crate::pty::Size>)
-        -> Result<Child>;
-}
+mod std;
 
-impl Command for std::process::Command {
+pub trait Command<T> {
     fn spawn_pty(
         &mut self,
         size: Option<&crate::pty::Size>,
-    ) -> Result<Child> {
-        let pty = crate::pty::Pty::new()?;
-        if let Some(size) = size {
-            pty.resize(size)?;
-        }
-
-        let pts = pty.pts()?;
-
-        let pt_fd = pty.pt().as_raw_fd();
-        let pts_fd = pts.as_raw_fd();
-
-        let stdin = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
-        let stdout = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
-        let stderr = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
-
-        // safe because the fds are valid (otherwise pty.pts() or dup() would
-        // have returned an Err and we would have exited early) and are not
-        // owned by any other structure (since dup() returns a fresh copy of
-        // the file descriptor), allowing from_raw_fd to take ownership of it.
-        self.stdin(unsafe { std::process::Stdio::from_raw_fd(stdin) })
-            .stdout(unsafe { std::process::Stdio::from_raw_fd(stdout) })
-            .stderr(unsafe { std::process::Stdio::from_raw_fd(stderr) });
-
-        // safe because setsid() and close() are async-signal-safe functions
-        // and ioctl() is a raw syscall (which is inherently
-        // async-signal-safe).
-        unsafe {
-            self.pre_exec(move || {
-                nix::unistd::setsid().map_err(|e| e.as_errno().unwrap())?;
-                set_controlling_terminal(&pts)
-                    .map_err(|e| e.as_errno().unwrap())?;
-
-                // in the parent, destructors will handle closing these file
-                // descriptors (other than pt, used by the parent to
-                // communicate with the child) when the function ends, but in
-                // the child, we end by calling exec(), which doesn't call
-                // destructors.
-
-                // XXX unwrap
-                nix::unistd::close(pt_fd)
-                    .map_err(|e| e.as_errno().unwrap())?;
-                nix::unistd::close(pts_fd)
-                    .map_err(|e| e.as_errno().unwrap())?;
-                // at this point, stdin/stdout/stderr have already been
-                // reopened as fds 0/1/2 in the child, so we can (and should)
-                // close the originals
-                nix::unistd::close(stdin)
-                    .map_err(|e| e.as_errno().unwrap())?;
-                nix::unistd::close(stdout)
-                    .map_err(|e| e.as_errno().unwrap())?;
-                nix::unistd::close(stderr)
-                    .map_err(|e| e.as_errno().unwrap())?;
-
-                Ok(())
-            });
-        }
-
-        let child = self.spawn().map_err(Error::Spawn)?;
-
-        Ok(Child { child, pty })
-    }
+    ) -> Result<Child<T>>;
 }
 
-pub struct Child {
-    child: std::process::Child,
+pub struct Child<T> {
+    child: T,
     pty: crate::pty::Pty,
 }
 
-impl Child {
-    pub fn pty(&self) -> &std::fs::File {
+impl<T> Child<T> {
+    pub fn pty(&self) -> &::std::fs::File {
         self.pty.pt()
     }
 
@@ -90,18 +26,80 @@ impl Child {
     }
 }
 
-impl std::ops::Deref for Child {
-    type Target = std::process::Child;
+impl<T> ::std::ops::Deref for Child<T> {
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.child
     }
 }
 
-impl std::ops::DerefMut for Child {
+impl<T> ::std::ops::DerefMut for Child<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.child
     }
+}
+
+fn setup_pty(
+    size: Option<&crate::pty::Size>,
+) -> Result<(
+    crate::pty::Pty,
+    ::std::fs::File,
+    ::std::os::unix::io::RawFd,
+    ::std::os::unix::io::RawFd,
+    ::std::os::unix::io::RawFd,
+)> {
+    let pty = crate::pty::Pty::new()?;
+    if let Some(size) = size {
+        pty.resize(size)?;
+    }
+
+    let pts = pty.pts()?;
+    let pts_fd = pts.as_raw_fd();
+
+    let stdin = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
+    let stdout = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
+    let stderr = nix::unistd::dup(pts_fd).map_err(Error::SpawnNix)?;
+
+    Ok((pty, pts, stdin, stdout, stderr))
+}
+
+fn pre_exec(
+    pt_fd: ::std::os::unix::io::RawFd,
+    pts_fd: ::std::os::unix::io::RawFd,
+    stdin: ::std::os::unix::io::RawFd,
+    stdout: ::std::os::unix::io::RawFd,
+    stderr: ::std::os::unix::io::RawFd,
+) -> ::std::io::Result<()> {
+    nix::unistd::setsid().map_err(|e| e.as_errno().unwrap())?;
+    set_controlling_terminal(pts_fd).map_err(|e| e.as_errno().unwrap())?;
+
+    // in the parent, destructors will handle closing these file
+    // descriptors (other than pt, used by the parent to
+    // communicate with the child) when the function ends, but in
+    // the child, we end by calling exec(), which doesn't call
+    // destructors.
+
+    // XXX unwrap
+    nix::unistd::close(pt_fd).map_err(|e| e.as_errno().unwrap())?;
+    nix::unistd::close(pts_fd).map_err(|e| e.as_errno().unwrap())?;
+    // at this point, stdin/stdout/stderr have already been
+    // reopened as fds 0/1/2 in the child, so we can (and should)
+    // close the originals
+    nix::unistd::close(stdin).map_err(|e| e.as_errno().unwrap())?;
+    nix::unistd::close(stdout).map_err(|e| e.as_errno().unwrap())?;
+    nix::unistd::close(stderr).map_err(|e| e.as_errno().unwrap())?;
+
+    Ok(())
+}
+
+fn set_controlling_terminal(
+    fd: ::std::os::unix::io::RawFd,
+) -> nix::Result<()> {
+    // safe because std::fs::File is required to contain a valid file
+    // descriptor
+    unsafe { set_controlling_terminal_unsafe(fd, ::std::ptr::null()) }
+        .map(|_| ())
 }
 
 nix::ioctl_write_ptr_bad!(
@@ -109,11 +107,3 @@ nix::ioctl_write_ptr_bad!(
     libc::TIOCSCTTY,
     libc::c_int
 );
-
-fn set_controlling_terminal(file: &std::fs::File) -> nix::Result<()> {
-    let fd = file.as_raw_fd();
-    // safe because std::fs::File is required to contain a valid file
-    // descriptor
-    unsafe { set_controlling_terminal_unsafe(fd, std::ptr::null()) }
-        .map(|_| ())
-}
---
+++ src/command/std.rs
@@ -0,0 +1,37 @@
+use crate::error::*;
+
+use std::os::unix::io::{AsRawFd as _, FromRawFd as _};
+use std::os::unix::process::CommandExt as _;
+
+impl super::Command<std::process::Child> for std::process::Command {
+    fn spawn_pty(
+        &mut self,
+        size: Option<&crate::pty::Size>,
+    ) -> Result<super::Child<std::process::Child>> {
+        let (pty, pts, stdin, stdout, stderr) = super::setup_pty(size)?;
+
+        let pt_fd = pty.pt().as_raw_fd();
+        let pts_fd = pts.as_raw_fd();
+
+        // safe because the fds are valid (otherwise pty.pts() or dup() would
+        // have returned an Err and we would have exited early) and are not
+        // owned by any other structure (since dup() returns a fresh copy of
+        // the file descriptor), allowing from_raw_fd to take ownership of it.
+        self.stdin(unsafe { std::process::Stdio::from_raw_fd(stdin) })
+            .stdout(unsafe { std::process::Stdio::from_raw_fd(stdout) })
+            .stderr(unsafe { std::process::Stdio::from_raw_fd(stderr) });
+
+        // safe because setsid() and close() are async-signal-safe functions
+        // and ioctl() is a raw syscall (which is inherently
+        // async-signal-safe).
+        unsafe {
+            self.pre_exec(move || {
+                super::pre_exec(pt_fd, pts_fd, stdin, stdout, stderr)
+            });
+        }
+
+        let child = self.spawn().map_err(Error::Spawn)?;
+
+        Ok(super::Child { child, pty })
+    }
+}
