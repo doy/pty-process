31d3a1ffa9879cc738994e224b871d09468dcd38
Jesse Luehrs <doy@tozt.net>
2023-12-03 03:05:46 -0500

    wip

--- tests/basic.rs
+++ tests/basic.rs
@@ -31,9 +31,11 @@ async fn test_cat_async() {
     use tokio::io::AsyncWriteExt as _;
 
     let mut pty = pty_process::Pty::new().unwrap();
-    let pts = pty.pts().unwrap();
-    pty.resize(pty_process::Size::new(24, 80)).unwrap();
-    let mut child = pty_process::Command::new("cat").spawn(&pts).unwrap();
+    let mut child = {
+        let pts = pty.pts().unwrap();
+        pty.resize(pty_process::Size::new(24, 80)).unwrap();
+        pty_process::Command::new("cat").spawn(&pts).unwrap()
+    };
 
     let (pty_r, mut pty_w) = pty.split();
 
@@ -54,9 +56,11 @@ async fn test_yes_async() {
     use tokio::io::AsyncReadExt as _;
 
     let mut pty = pty_process::Pty::new().unwrap();
-    let pts = pty.pts().unwrap();
-    pty.resize(pty_process::Size::new(24, 80)).unwrap();
-    let mut child = pty_process::Command::new("yes").spawn(&pts).unwrap();
+    let mut child = {
+        let pts = pty.pts().unwrap();
+        pty.resize(pty_process::Size::new(24, 80)).unwrap();
+        pty_process::Command::new("yes").spawn(&pts).unwrap()
+    };
 
     let mut buf = [0u8; 3];
 
